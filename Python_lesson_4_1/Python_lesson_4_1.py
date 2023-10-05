fec8ef72e402fce720c1da38b12810cda32cd8b0
Utkinskii <sakunov1707@gmail.com>
2023-10-05 13:01:16 +0300

    Добавьте файлы проекта.

---
+++ Python_lesson_4_1/Python_lesson_4_1.py
@@ -0,0 +1,6 @@
+a = float(input("Enter the value of side a: "))
+b = float(input("Enter the value of side b: "))
+ploshad = a * b
+perimetr = 2 * (a + b)
+print("ploshad =", ploshad)
+print("perimetr =", perimetr)
